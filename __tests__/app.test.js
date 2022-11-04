e07acbf1ee3b1fa41e7ef91f6d4b7bc762829db8
Connor McCue <connor.mccue@comcast.net>
2022-11-04 12:20:30 -0700

    test 1

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+
+
+  it('/boats should return a list of boats with stats', async () => {
+    const res = await request(app).get('/boats');
+    const expected = boats.map((boat) => {
+      return { id: boat.id, name: boat.name };
+    });
+    expect(res.body).toEqual(expected);
   });
+
+
+
+
   afterAll(() => {
     pool.end();
   });
