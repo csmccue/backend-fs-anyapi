1b2f9dcd661b4af78ce411af9081bdf1cdd8cb9b
Connor McCue <connor.mccue@comcast.net>
2022-11-04 16:28:39 -0700

    I manually input into my table, just realized I need to use SQL per rubric - built out setup.sql to create table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,14 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS boats;
+
+CREATE TABLE boats (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  type VARCHAR NOT NULL
+);
+
+INSERT INTO boats (name, type)
+VALUES
+('USS Atlanta', 'Light Cruiser'),
+('USS Indianapolis', 'Heavy Cruiser');
