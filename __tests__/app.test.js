bf978cefa56748e74dbc2154035fdf390e5edf98
Connor McCue <connor.mccue@comcast.net>
2022-11-04 12:28:31 -0700

    boat data added to database for calls and utils boats-data for testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { boats } = require('../lib/utils/boats-data');
 
 
 describe('backend-express-template routes', () => {
